e3cb1e996d2dcb2f1b7a29e7b6f4f55b937f9d42
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-09-12 12:31:47 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,8 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
-      "IBM Plex Sans": {
-        wght: [400, 500, 700],
-        ital: [400, 500, 700],
-      },
-      "IBM Plex Serif": {
-        wght: [400, 500, 700],
-        ital: [400, 500, 700],
-      },
+      "IBM Plex Sans": true,
+      "IBM Plex Serif": true,
       Cousine: true,
     },
   },
